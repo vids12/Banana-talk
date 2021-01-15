a1f2fc51cf447cb39ce52c2444a2855d3d401db1
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-01-16 02:18:29 +0530

    Input output done

---
+++ app.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText= document.querySelector("#text-input");
+btnTranslate.addEventListener("click", clickHandler);
+var textOutput = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function fetchUrl(input){
+    return serverURL +"?" +"text=" + input;
+}
+
+function clickHandler() {
+    var textInput = inputText.value;
+    fetch(fetchUrl(textInput))
+    .then(response =>  response.json)
+    .then(json => {var translatedText = json.contents.translated;
+    textOutput.innerText=translatedText;});
+    .catch(errorHandler)
+};
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something went wrong..Try after some time");
+}
